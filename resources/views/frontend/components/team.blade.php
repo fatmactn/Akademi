5c5b458f4c09ba3dc53b6c72f2a6914080d715f0
mmstfkc <36615411+mmstfkc@users.noreply.github.com>
2021-10-06 15:05:47 +0300

    sliders and team

--- resources/views/frontend/components/team.blade.php
+++ resources/views/frontend/components/team.blade.php
@@ -14,186 +14,26 @@
         <div class="row">
             <div class="team-items">
 
+                @foreach($employees as  $employee)
                 <div class="col-md-2">
                     <div class="team-container wow bounceIn" data-wow-delay="0.2s">
                         <div class="team-item">
                             <div class="team-triangle">
                                 <div class="content">
-                                    <img src="{{asset('frontend/img/team/1.jpg')}}" alt="title"/>
+                                    <img src="{{asset($employee->imageUrl)}}" alt="title"/>
                                     <div class="team-hover text-center">
                                         <i class="fa fa-user"></i>
-                                        <p>Merthan Kuzeyli </p>
-                                        <p>Kurucu</p>
+                                        <p>{{$employee->nameSurname}} </p>
+                                        <p>{{$employee->degree}}</p>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-2">
-                    <div class="team-container wow bounceIn" data-wow-delay="0.3s">
-                        <div class="team-item">
-                            <div class="team-triangle">
-                                <div class="content">
-                                    <img src="img/team/1.jpg" alt="title"/>
-                                    <div class="team-hover text-center">
-                                        <i class="fa fa-user"></i>
-                                        <p>Cevdet Ertürk</p>
-                                        <p>Kurucu</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-2">
-                    <div class="team-container wow bounceIn" data-wow-delay="0.4s">
-                        <div class="team-item">
-                            <div class="team-triangle">
-                                <div class="content">
-                                    <img src="img/team/1.jpg" alt="title"/>
-                                    <div class="team-hover text-center">
-                                        <i class="fa fa-user"></i>
-                                        <p>Gökmen Bolayır</p>
-                                        <p>Kurucu</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                @endforeach
 
-                <div class="col-md-2">
-                    <div class="team-container wow bounceIn" data-wow-delay="0.5s">
-                        <div class="team-item">
-                            <div class="team-triangle">
-                                <div class="content">
-                                    <img src="img/team/1.jpg" alt="title"/>
-                                    <div class="team-hover text-center">
-                                        <i class="fa fa-user"></i>
-                                        <p>Deniz Özcan</p>
-                                        <p>Kurucu</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-2">
-                    <div class="team-container wow bounceIn" data-wow-delay="0.3s">
-                        <div class="team-item">
-                            <div class="team-triangle">
-                                <div class="content">
-                                    <img src="img/team/1.jpg" alt="title"/>
-                                    <div class="team-hover text-center">
-                                        <i class="fa fa-user"></i>
-                                        <p>Ali Doğan</p>
-                                        <p>Yazılım Geliştirici</p>
-                                        <p style="font-size:10px;">(Fullstack Developer)</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-2">
-                    <div class="team-container wow bounceIn" data-wow-delay="0.7s">
-                        <div class="team-item">
-                            <div class="team-triangle">
-                                <div class="content">
-                                    <img src="img/team/1.jpg" alt="title"/>
-                                    <div class="team-hover text-center">
-                                        <i class="fa fa-user"></i>
-                                        <p>Fatih Burgaz</p>
-                                        <p>Yazılım Geliştirici</p>
-                                        <p style="font-size:10px;">(Backend Developer)</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-2">
-                    <div class="team-container wow bounceIn" data-wow-delay="0.7s">
-                        <div class="team-item">
-                            <div class="team-triangle">
-                                <div class="content">
-                                    <img src="img/team/1.jpg" alt="title"/>
-                                    <div class="team-hover text-center">
-                                        <i class="fa fa-user"></i>
-                                        <p>Ceren Savaş</p>
-                                        <p>Yazılım Geliştirici</p>
-                                        <p style="font-size:10px;">(Frontend Developer)</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-2">
-                    <div class="team-container wow bounceIn" data-wow-delay="0.7s">
-                        <div class="team-item">
-                            <div class="team-triangle">
-                                <div class="content">
-                                    <img src="img/team/1.jpg" alt="title"/>
-                                    <div class="team-hover text-center">
-                                        <i class="fa fa-user"></i>
-                                        <p>Burcu Atalay</p>
-                                        <p>Teknik Destek</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-2">
-                    <div class="team-container wow bounceIn" data-wow-delay="0.7s">
-                        <div class="team-item">
-                            <div class="team-triangle">
-                                <div class="content">
-                                    <img src="img/team/1.jpg" alt="title"/>
-                                    <div class="team-hover text-center">
-                                        <i class="fa fa-user"></i>
-                                        <p>Hüseyin Bey</p>
-                                        <p>Operasyon Birimi</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-2">
-                    <div class="team-container wow bounceIn" data-wow-delay="0.7s">
-                        <div class="team-item">
-                            <div class="team-triangle">
-                                <div class="content">
-                                    <img src="img/team/1.jpg" alt="title"/>
-                                    <div class="team-hover text-center">
-                                        <i class="fa fa-user"></i>
-                                        <p>Hilal Yalçın</p>
-                                        <p>?</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-2 col-sm-12">
-                    <div class="team-container wow bounceIn" data-wow-delay="0.7s">
-                        <div class="team-item">
-                            <div class="team-triangle">
-                                <div class="content">
-                                    <img src="img/team/1.jpg" alt="title"/>
-                                    <div class="team-hover text-center">
-                                        <i class="fa fa-user"></i>
-                                        <p>Şehriban ?</p>
-                                        <p>Ofis Personeli</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+
                 <div class="clearfix"></div>
             </div>
         </div>

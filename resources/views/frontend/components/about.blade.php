72218675e3b89816f710fb9e579c80a3062290ab
mmstfkc <36615411+mmstfkc@users.noreply.github.com>
2021-10-07 14:20:40 +0300

    Apis updates

--- resources/views/frontend/components/about.blade.php
+++ resources/views/frontend/components/about.blade.php
@@ -28,37 +28,6 @@
                     @endif
                 @endforeach
 
-<!--
-                <div class="col-md-6 col-sm-12">
-                    <a href="#" class="rotate-box-1 square-icon wow zoomIn" data-wow-delay="0.2s">
-                        <span class="rotate-box-icon"><i class="fa fa-dot-circle-o"></i></span>
-                        <div class="rotate-box-info">
-                            <h4>Hedefimiz</h4>
-                            <p>Lorem ipsum dolor sit amet set, consoranet adipisicing elit, s tempor incidist.Lorem ipsum dolor sit amet set, consectetur utes anet adipisicing elit, sed do eiusmod tempor incidist.Lorem ipsum dolor sit amet set, consectetur utes anet adipisicing elit, sed do eiusmod tempor incidist.Lorem ipsum dolor sit amet set, consectetur utes anet adipisicing elit, sed do eiusmod tempor incidist.Lorem ipsum doliusmod tempor incidist.</p>
-                        </div>
-                    </a>
-                </div>
-
-                <div class="col-md-6 col-sm-12">
-                    <a href="#" class="rotate-box-1 square-icon wow zoomIn" data-wow-delay="0.4s">
-                        <span class="rotate-box-icon"><i class="fa fa-graduation-cap"></i></span>
-                        <div class="rotate-box-info">
-                            <h4>Eğitim Süreci</h4>
-                            <p>Lorem ipsum dolor sit amet set, consectetur utes anet adipisicing elit, sed do eiusmod tempor incidist.</p>
-                        </div>
-                    </a>
-                </div>
-
-                <div class="col-md-6 col-sm-12">
-                    <a href="#" class="rotate-box-1 square-icon wow zoomIn" data-wow-delay="0.6s">
-                        <span class="rotate-box-icon"><i class="fa fa-briefcase"></i></span>
-                        <div class="rotate-box-info">
-                            <h4>Akademi Sonrası Süreç</h4>
-                            <p>Lorem ipsum dolor sit amet set, consectetur utes anet adipisicing elit, sed do eiusmod tempor incidist.</p>
-                        </div>
-                    </a>
-                </div>
--->
             </div> <!-- /.row -->
         </div> <!-- /.container -->
     </div>

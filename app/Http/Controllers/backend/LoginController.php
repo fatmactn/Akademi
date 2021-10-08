dacebe36500f40773e3227ab6a047f2f06afb267
mmstfkc <36615411+mmstfkc@users.noreply.github.com>
2021-10-08 10:25:36 +0300

    login check settings

--- app/Http/Controllers/backend/LoginController.php
+++ app/Http/Controllers/backend/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Backend;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class LoginController extends Controller
 {
@@ -20,13 +21,25 @@ class LoginController extends Controller
             'password' => 'required',
         ]);
 
-        $credentials = $request->only('email', 'password');
+//        $credentials = $request->only('email', 'password');
 
-        if (Auth::attempt($credentials)) {
-            return redirect()->route('backend.home.index');
+
+        $login_information = User::where('email', $request->email)->first();
+        if ($login_information) {
+            if ($login_information->status == 1) {
+                if ($login_information->password == $request->password) {
+                    return redirect()->route('backend.home.index');
+                } else {
+                    return redirect()->route('backend.login.index')->with('fail', 'Parola Hatalı.');
+                }
+            } else {
+                return redirect()->route('backend.login.index')->with('fail', 'Yetkiniz yok.');
+            }
+
         } else {
-            return redirect()->route('backend.login.index')->with('fail','Yetkiniz yok.');
+            return redirect()->route('backend.login.index')->with('fail', 'Kullanıcı yok.');
         }
+
     }
 
 
